ec6c91796326109aef0b058303b509ac6bd23927
Walter Smuts <wssmts@amazon.com>
2021-11-22 08:13:44 +0200

    Fix spelling of length

--- chessclient.py
+++ chessclient.py
@@ -10,29 +10,29 @@ class ChessClient:
         self.socket.connect((server, port))
         print("Connected to server: {} at port {}".format(server, port))
 
-    def send_lenth_prefixed(self, data):
+    def send_length_prefixed(self, data):
         self.socket.send(len(data).to_bytes(1, ENDIANNESS))
         self.socket.send(data)
 
-    def recv_lenth_prefixed(self):
+    def recv_length_prefixed(self):
         length = int.from_bytes(self.socket.recv(1), ENDIANNESS)
         return self.socket.recv(length)
 
     def run(self):
-        self.send_lenth_prefixed(str(self.opponent).encode('utf-8'))
+        self.send_length_prefixed(str(self.opponent).encode('utf-8'))
         if self.opponent == "Network":
             print("Waiting for another network opponent to  connect...")
         while True:
-            data = self.recv_lenth_prefixed().decode("utf-8")
+            data = self.recv_length_prefixed().decode("utf-8")
             if len(data) == 1:
                 break
             print(data)
 
             move = self.player.get_move(data)
-            self.send_lenth_prefixed(str(move).encode('utf-8'))
+            self.send_length_prefixed(str(move).encode('utf-8'))
 
         result = data
-        data = self.recv_lenth_prefixed().decode("utf-8")
+        data = self.recv_length_prefixed().decode("utf-8")
 
         print(data)
         if result == "W":
@@ -44,6 +44,6 @@ class ChessClient:
         else:
             print("Unknown control message.")
 
-        url = self.recv_lenth_prefixed().decode("utf-8")
+        url = self.recv_length_prefixed().decode("utf-8")
         print("Find your game at:");
         print(url);
